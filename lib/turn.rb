6617628a5db40f17832042232844e0c3d47e4d57
Will Figueiredo <will@wfigueiredo.com>
2018-08-17 03:37:26 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,31 @@
 def turn()
 
 end
+
+def display_board(board)
+
+end
+
+# Determing is the chosen move is valid
+def valid_move?(board, index)
+  if (position_taken?(board, index) == false && index.between?(0, 8) == true)
+    return true
+  else
+    return false
+  end
+end
+
+# Find out if the chosen position is taken
+def position_taken?(board, index)
+  position = board[index]
+  if position == " " || position == "" || position == nil
+    return false
+  else
+    return true
+  end
+end
+
+# Make the move
+def move(board, index)
+
+end
